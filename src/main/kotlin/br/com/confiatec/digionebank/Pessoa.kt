e2e064b70e11462cd66102f64b4ca20efe220a96
Ednei Almeida <ednei@confiatec.com.br>
2021-02-18 16:46:52 -0300

    membros da classe

--- src/main/kotlin/br/com/confiatec/digionebank/Pessoa.kt
+++ src/main/kotlin/br/com/confiatec/digionebank/Pessoa.kt
@@ -3,10 +3,17 @@ package br.com.confiatec.digionebank
 class Pessoa {
     var nome: String = "Ed"
     var cpf: String = "12345678900"
+    private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 fun main() {
     val ed = Pessoa()
-    println(ed.nome)
+    println(ed.pessoaInfo())
     println(ed.cpf)
+
+    println(ed.nome)
 }

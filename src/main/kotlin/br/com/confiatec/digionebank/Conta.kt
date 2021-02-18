0477b019e990f376654daf0159fd95d1adf71c39
Ednei Almeida <ednei@confiatec.com.br>
2021-02-18 16:58:38 -0300

    classe Conta

---
+++ src/main/kotlin/br/com/confiatec/digionebank/Conta.kt
@@ -0,0 +1,16 @@
+package br.com.confiatec.digionebank
+
+import java.math.BigDecimal
+
+class Conta (
+    val agencia: String,
+    val numero: String,
+    val saldo: BigDecimal
+) {
+   fun deposito(valor: BigDecimal) {
+
+   }
+    fun saque(valor: BigDecimal) {
+
+    }
+}

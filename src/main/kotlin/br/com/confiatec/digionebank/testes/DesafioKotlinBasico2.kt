80f0ad6f8382bf795828ada5804847b4341d8ad8
Ednei Almeida <ednei@confiatec.com.br>
2021-02-22 05:51:26 -0300

    Desafio Básico 2

---
+++ src/main/kotlin/br/com/confiatec/digionebank/testes/DesafioKotlinBasico2.kt
@@ -0,0 +1,7 @@
+package br.com.confiatec.digionebank.testes
+
+fun main(args: Array<String>) {
+
+    for (i in 1.rangeTo(readLine()!!.toInt()).step( 2  )) println(i) //complete o codigo com os valores corretos
+
+}
